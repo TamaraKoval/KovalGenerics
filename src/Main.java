c729057718016d3f7024e47607920d79a90c7f16
TamaraKoval <kovalpsy@mail.ru>
2023-02-02 10:09:59 +0300

    Draft Magic Box

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,4 @@
+import boxes.MagicBox;
+
 public class Main {
 }
--- src/boxes/MagicBox.java
+++ src/boxes/MagicBox.java
@@ -1,4 +1,41 @@
 package boxes;
 
-public class MagicBox {
+import java.util.Random;
+
+public class MagicBox<T> {
+    protected T[] items;
+
+    public T[] getItems() {
+        return items;
+    }
+
+    public MagicBox(int maxItems) {
+        this.items = (T[]) new Object[maxItems];
+    }
+
+    public boolean add (T item) {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                items[i] = item;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public T pick() throws Exception {
+        int k = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                k++;
+            }
+            if (k > 0) {
+                throw new Exception("Коробка не заполнена, осталось заполнить ячеек: " + k);
+            } else {
+                Random random = new Random();
+                int randomInt = random.nextInt(items.length);
+                return items[randomInt];
+            }
+        }
+    }
 }
