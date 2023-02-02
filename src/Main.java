230f202f72163e37b3e712e1506428ea9fe28331
TamaraKoval <kovalpsy@mail.ru>
2023-02-02 10:46:12 +0300

    Change Type Pick in MagicBox and test in Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 import boxes.MagicBox;
 
 public class Main {
-    
+    public static void main(String[] args) throws Exception {
+        MagicBox<Integer> testBox1 = new MagicBox<>(3); // тест на Integer
+        testBox1.add(4); // заполняем первую ячейку коробки
+        testBox1.add(5); // заполняем вторую ячейку коробки
+        System.out.println(testBox1); // смотрим содержимое коробки
+        // testBox.pick(); // программа выкинула Ошибку, все работает корректно
+        testBox1.add(6); // заполнили полностью коробку
+        System.out.println(testBox1); // смотрим содержимое коробки
+        testBox1.pick(); // магическая коробка вернула рандомный элемент массива
+
+        System.out.println();
+
+        MagicBox<String> testBox2 = new MagicBox<>(4); // тест на String
+        testBox2.add("Ехал");
+        testBox2.add("Грека");
+        testBox2.add("через");
+        testBox2.add("реку"); // заполнили полностью коробку
+        System.out.println(testBox2); // смотрим содержимое коробки
+        testBox2.pick(); // магическая коробка вернула рандомный элемент массива
+
+    }
 }
--- src/boxes/MagicBox.java
+++ src/boxes/MagicBox.java
@@ -1,5 +1,6 @@
 package boxes;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class MagicBox<T> {
@@ -13,7 +14,12 @@ public class MagicBox<T> {
         this.items = (T[]) new Object[maxItems];
     }
 
-    public boolean add (T item) {
+    @Override
+    public String toString() {
+        return ("В коробке сейчас лежат: " + Arrays.toString(items));
+    }
+
+    public boolean add(T item) {
         for (int i = 0; i < items.length; i++) {
             if (items[i] == null) {
                 items[i] = item;
@@ -23,7 +29,7 @@ public class MagicBox<T> {
         return false;
     }
 
-    public T pick() throws Exception {
+    public void pick() throws Exception {
         int k = 0;
         for (int i = 0; i < items.length; i++) {
             if (items[i] == null) {
@@ -35,7 +41,7 @@ public class MagicBox<T> {
         } else {
             Random random = new Random();
             int randomInt = random.nextInt(items.length);
-            return items[randomInt];
+            System.out.println("Магическая коробка решила вернуть тебе: " + items[randomInt]);
         }
     }
 }
